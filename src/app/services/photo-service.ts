d4fb4d27ef7ec0804705c73a4e7ad053f766c26f
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2025-09-26 17:00:05 +0100

    feat: display taken photos

--- src/app/services/photo-service.ts
+++ src/app/services/photo-service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
-import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import {
+  Camera,
+  CameraResultType,
+  CameraSource,
+} from '@capacitor/camera';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PhotoService {
+  // Array of photos
+  public photos: UserPhoto[] = [];
+
   public async addNewToGalley() {
     // Take a photo
     const capturePhoto = await Camera.getPhoto({
@@ -12,5 +19,16 @@ export class PhotoService {
       source: CameraSource.Camera,
       quality: 100,
     });
+
+    // Add the newly captured photo to the beginning of the photos array
+    this.photos.unshift({
+      filepath: 'soon...',
+      webviewPath: capturePhoto.webPath!,
+    });
   }
+}
+
+export interface UserPhoto {
+  filepath: string;
+  webviewPath?: string;
 }
