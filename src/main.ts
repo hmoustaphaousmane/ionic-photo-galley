ad4fecc75e5ac3ca8ada367296c726c4b8f63ed2
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2025-09-26 14:49:41 +0100

    build: install capacitor plugins and pwa-elements
    - install Capacitor plugins to make the app's native
    - inst pwa elements to enable web-based functionality and UI via the Ionic PWA Elements library

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+// Call the element loader
+defineCustomElements(window);
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.log(err));
